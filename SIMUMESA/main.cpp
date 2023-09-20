9fad8f106cf3f788fef0e2fcca241ceb69298a32
Agustin Herrera <agusveda110303@gmail.com>
2023-09-19 21:05:16 -0300

    debug

--- SIMUMESA/main.cpp
+++ SIMUMESA/main.cpp
@@ -5,12 +5,8 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 
     menu_general();
 
-=======
-    cout << "HOLA MUNDO" << endl;
->>>>>>> d19c65164fd784aa314e6c4d4201ce4d5966287e
     return 0;
 }
