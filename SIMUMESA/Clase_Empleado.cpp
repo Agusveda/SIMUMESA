e3825d89d9bc7d679710f8d7bedff932782a409d
AndresBntz <107317720+AndresBntz@users.noreply.github.com>
2023-10-10 18:15:43 -0300

    ClassEmpleado

--- SIMUMESA/Clase_Empleado.cpp
+++ SIMUMESA/Clase_Empleado.cpp
@@ -1,28 +1,85 @@
 #include<cstring>
 #include<cstdlib>
 #include <iostream>
-/// acordarse de la funcion cargar cadena para los char.
-/// funcion menu
-/*
-void cargar_cadena(char *palabra, int tam){
-    int i = 0;
-    fflush(stdin);
-    for (i = 0 ; i < tam ; i++ ){
-        palabra[i] = cin.get();
-        if(palabra[i] == '\n') break;
-    }
-    palabra[i] = '\0';
-    fflush(stdin);
-}
- */
 using namespace std;
+#include "Clase_Empleado.h"
+#include "Cargar_Cadena.h"
 
-int main() {
+    ///SETS
+    void Empleado:: setLegajo(int Legajo){_Legajo=Legajo;}
+    void Empleado:: setNombreEmpleado(const char *NombreEmpleado){strcpy(_NombreEmpleado,NombreEmpleado);}
+    void Empleado:: setApellidoEmpleado(const char *ApellidoEmpleado){strcpy(_ApellidoEmpleado,ApellidoEmpleado);}
+    void Empleado:: setCargo(int Cargo){_Cargo=Cargo;}
 
-   return 0;
-}#include "Clase_Empleado.h"
+    ///GETS
+    int Empleado::getLegajo(){return _Legajo;}
+    const char *Empleado::getNombreEmpleado(){return _NombreEmpleado;}
+    const char *Empleado::getApellidoEmpleado(){return _ApellidoEmpleado;}
+    int Empleado::getCargo(){return _Cargo;}
 
-Clase_Empleado::Clase_Empleado()
-{
-    //ctor
+
+
+    ///AGREGA EMPLEADO
+    void Empleado::AgregarEmpleado()
+    {
+        cout << "introducir Legajo de la Hamburguesa" <<endl;
+        cin >> _Legajo;
+        cout << "introducir nombre de empleado : "<<endl;
+        cargar_cadena(_NombreEmpleado, 29);
+        cout << "introducir Apellid de empleado :"<<endl;
+        cargar_cadena(_ApellidoEmpleado, 29);
+        cout<< "Ingresar el cargo "<<endl;
+        cin>>_Cargo;
+    }
+
+
+    ///MUESTRA EMPLEADO
+    void Empleado::MostrarEmpleado()
+    {
+       cout << "Legajo: "<<_Legajo<<endl;
+       cout << "Nombre: "<<_NombreEmpleado<<endl;
+       cout << "Apellido: "<<_ApellidoEmpleado<<endl;
+       cout << "Cargo: "<<_Cargo<<endl;
+    }
+
+
+
+    ///GRABAR ARCHIVO EMPLEADO
+    bool ArchivoEmpleado::GrabarRegistroEmpleado(){
+    Empleado registro;
+    FILE *Emp;
+    Emp = fopen("Empleado.dat", "ab");
+    if (Emp == nullptr){
+        cout << " ERROR DE ARCHIVO" << endl;
+        system("pause");
+        return false;
+    }
+
+    cout << "INGRESAR LOS VALORES DEL REGISTRO"<< endl;
+    registro.AgregarEmpleado();
+    bool escribio = fwrite(&registro , sizeof registro, 1 , Emp);
+    fclose(Emp);
+    return escribio;
+    }
+
+
+
+    ///MOSTRAR EL ARCHIVO
+    bool Mostrarregistros(){
+    Empleado reg;
+    FILE *Emp;
+    Emp= fopen("Empleado.dat","rb");
+       if(Emp==NULL){
+    cout<< "ERROR AL ABRIR EL ARCHIVO "<<endl;
+    return false;
+    }
+    while(fread (&reg,sizeof reg, 1,Emp)==1){
+    reg.MostrarEmpleado();
+    cout<<endl;
+	}
+	fclose(Emp);
+    return true;
 }
+
+
+
--- SIMUMESA/Clase_Empleado.h
+++ SIMUMESA/Clase_Empleado.h
@@ -2,14 +2,49 @@
 #define CLASE_EMPLEADO_H
 
 
-class Clase_Empleado
-{
-    public:
-        Clase_Empleado();
-
-    protected:
-
+class Empleado{
     private:
+        int _Legajo;
+        char _NombreEmpleado[30], _ApellidoEmpleado[30];
+        int _Cargo;
+
+    public:
+        ///CONTRUCTOR
+       Empleado(){
+            strcpy(_NombreEmpleado, "");
+            strcpy (_ApellidoEmpleado, "");
+            _Legajo = 0;
+            _Cargo = 0;}
+
+
+    ///SETS
+    void setLegajo(int Legajo);
+    void setNombreEmpleado(const char *NombreEmpleado);
+    void setApellidoEmpleado(const char *ApellidoEmpleado);
+    void setCargo(int Cargo);
+
+    ///GETS
+    int getLegajo();
+    const char *getNombreEmpleado();
+    const char *getApellidoEmpleado();
+    int getCargo();
+
+
+    void AgregarEmpleado();
+    void MostrarEmpleado();
+
+};
+
+ class ArchivoEmpleado{
+    private:
+    public:
+
+    /// FUNCIONES
+        bool GrabarRegistroEmpleado();
+        bool MostrarRegistros();
+
 };
 
 #endif // CLASE_EMPLEADO_H
+
+
