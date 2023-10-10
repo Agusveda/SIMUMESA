319e44947ec4ced4b04bd25ed409b22d8256c2e9
Agustin Herrera <agusveda110303@gmail.com>
2023-10-10 15:33:18 -0300

    clase

---
+++ SIMUMESA/Clase_Empleado.cpp
@@ -0,0 +1,28 @@
+#include<cstring>
+#include<cstdlib>
+#include <iostream>
+/// acordarse de la funcion cargar cadena para los char.
+/// funcion menu
+/*
+void cargar_cadena(char *palabra, int tam){
+    int i = 0;
+    fflush(stdin);
+    for (i = 0 ; i < tam ; i++ ){
+        palabra[i] = cin.get();
+        if(palabra[i] == '\n') break;
+    }
+    palabra[i] = '\0';
+    fflush(stdin);
+}
+ */
+using namespace std;
+
+int main() {
+
+   return 0;
+}#include "Clase_Empleado.h"
+
+Clase_Empleado::Clase_Empleado()
+{
+    //ctor
+}
---
+++ SIMUMESA/Clase_Empleado.h
@@ -0,0 +1,15 @@
+#ifndef CLASE_EMPLEADO_H
+#define CLASE_EMPLEADO_H
+
+
+class Clase_Empleado
+{
+    public:
+        Clase_Empleado();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASE_EMPLEADO_H
